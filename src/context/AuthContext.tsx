99b286c13cbf7cecbf4f349c91f100ac7078c153
aungkanphyoo <aungkanphyo1@gmail.com>
2024-06-05 21:02:35 +0630

    ok stage implement '{} as AuthType' this in the createContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,12 @@
 import { createContext , useContext, useState } from "react";
-// interface AuthType  {
-//     user : string;
-//     setUser : React.Dispatch<React.SetStateAction<string>>;
-//     login : (data : string)=>void;
-//     logout : ()=>void
-// }
+type AuthType = {
+    user : string;
+    setUser : React.Dispatch<React.SetStateAction<string>>;
+    login : (data : string)=>void;
+    logout : ()=>void
+}
 
-const AuthContext = createContext('');
+const AuthContext = createContext({} as AuthType);
 export const AuthProvider = ({children} : {children:React.ReactNode})=>{
     const [user,setUser] = useState<string>('');
     const login = (data:string)=>{
